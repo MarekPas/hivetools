43e58f9c3fdb2aafd89b7c2b6a112e10ed25a209
MarekPas <58218081+MarekPas@users.noreply.github.com>
2021-06-22 15:29:10 +0300

    changed settings

--- hivetools/settings.py
+++ hivetools/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('hivetools_SC', ')bcmb5swumy^x%ucbha5*d(xn-0$mm#4n4)0jqe73@xzm(a6s3')
+SECRET_KEY = os.environ.get('hivetools_SC')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', False)
